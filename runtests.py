6a28e94b1c4192e1238b3fcf3105a208b018d5ab
Zbigniew Siciarz <antyqjon@gmail.com>
2013-12-23 19:02:46 +0100

    Test runner script.

---
+++ runtests.py
@@ -0,0 +1,30 @@
+from django.conf import settings
+
+if not settings.configured:
+    INSTALLED_APPS = (
+        'django.contrib.auth',
+        'django.contrib.contenttypes',
+        'django.contrib.sessions',
+        'django.contrib.messages',
+        'django_nose',
+        'markitup',
+        'sorl.thumbnail',
+        'pgallery',
+    )
+    settings.configure(
+        DATABASES={
+            'default': {
+                'ENGINE': 'django.db.backends.sqlite3',
+                'NAME': ':memory:',
+            }
+        },
+        INSTALLED_APPS=INSTALLED_APPS,
+        MARKITUP_FILTER=('markdown.markdown', {'safe_mode': False, 'extensions': ['codehilite']}),
+        MARKITUP_SET='markitup/sets/markdown',
+    )
+
+
+from django_nose import NoseTestSuiteRunner
+
+test_runner = NoseTestSuiteRunner()
+test_runner.run_tests(['pgallery'])
