65e52982ee3f73878cd991175797011c7bcbf521
Zbigniew Siciarz <antyqjon@gmail.com>
2013-12-25 11:38:12 +0100

    Refactored test setup into a base class.

--- pgallery/tests/test_models.py
+++ pgallery/tests/test_models.py
@@ -6,9 +6,9 @@ from django.test import TestCase
 from ..models import Gallery, Photo
 
 
-class GalleryModelTestCase(TestCase):
+class BaseTestCase(TestCase):
     def setUp(self):
-        super(GalleryModelTestCase, self).setUp()
+        super(BaseTestCase, self).setUp()
         User = get_user_model()
         self.user = User.objects.create_user(
             username='test',
@@ -20,6 +20,12 @@ class GalleryModelTestCase(TestCase):
             title="Test gallery",
             slug="test-gallery",
         )
+
+
+class GalleryModelTestCase(BaseTestCase):
+    """
+    Tests for ``Gallery`` model class.
+    """
 
     def test_str(self):
         """
