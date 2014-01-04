3a747a63223cbbadb886bee054a73b429f29ad0a
Zbigniew Siciarz <antyqjon@gmail.com>
2014-01-04 10:54:34 +0100

    Keep the tests away from database.

--- pgallery/tests/test_models.py
+++ pgallery/tests/test_models.py
@@ -1,37 +1,11 @@
 from __future__ import unicode_literals
 
-from django.contrib.auth import get_user_model
-from django.test import TestCase
-
-from djet.files import create_inmemory_image
+import unittest
 
 from ..models import Gallery, Photo
 
 
-class BaseTestCase(TestCase):
-    def setUp(self):
-        super(BaseTestCase, self).setUp()
-        User = get_user_model()
-        self.user = User.objects.create_user(
-            username='test',
-            email='test@example.org',
-            password='test',
-        )
-        self.gallery = Gallery.objects.create(
-            author=self.user,
-            title="Test gallery",
-            slug="test-gallery",
-        )
-        self.photo = Photo.objects.create(
-            gallery=self.gallery,
-            author=self.user,
-            title="Test photo",
-            image=create_inmemory_image('test.jpg', format='JPEG'),
-            exif={},
-        )
-
-
-class GalleryModelTestCase(BaseTestCase):
+class GalleryModelTestCase(unittest.TestCase):
     """
     Tests for ``Gallery`` model class.
     """
@@ -40,10 +14,11 @@ class GalleryModelTestCase(BaseTestCase):
         """
         Check that string representation of a gallery is its title.
         """
-        self.assertEqual(str(self.gallery), self.gallery.title)
+        gallery = Gallery(title="Test gallery")
+        self.assertEqual(str(gallery), gallery.title)
 
 
-class PhotoModelTestCase(BaseTestCase):
+class PhotoModelTestCase(unittest.TestCase):
     """
     Tests for ``Photo`` model class.
     """
@@ -52,4 +27,5 @@ class PhotoModelTestCase(BaseTestCase):
         """
         Check that string representation of a photo is its title.
         """
-        self.assertEqual(str(self.photo), self.photo.title)
+        photo = Photo(title="Test photo")
+        self.assertEqual(str(photo), photo.title)
