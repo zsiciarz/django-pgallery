6deab74e41cabcb9a3fb4075f270a9cdd591a435
Zbigniew Siciarz <antyqjon@gmail.com>
2014-01-14 21:34:45 +0100

    Test type coercion in sanitize_exif_value

--- pgallery/tests/test_utils.py
+++ pgallery/tests/test_utils.py
@@ -13,3 +13,11 @@ class SanitizeExifValueTestCase(unittest.TestCase):
         key = "not relevant"
         value = "abc\x00d"
         self.assertEqual(sanitize_exif_value(key, value), "abcd")
+
+    def test_coerce_to_text(self):
+        """
+        Check that non-text types are coerced to text.
+        """
+        key = "not relevant"
+        value = (20, 70)
+        self.assertEqual(sanitize_exif_value(key, value), "(20, 70)")
