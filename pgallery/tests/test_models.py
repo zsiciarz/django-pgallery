82e1a55617b28fe7927cbb5b5ea146eb23c6cb81
Zbigniew Siciarz <antyqjon@gmail.com>
2014-01-18 22:01:27 +0100

    Added User and Gallery factories for integration tests.

--- pgallery/tests/test_models.py
+++ pgallery/tests/test_models.py
@@ -2,7 +2,20 @@ from __future__ import unicode_literals
 
 import unittest
 
+from django.conf import settings
+
+import factory
+
 from ..models import Gallery, Photo
+
+
+class UserFactory(factory.django.DjangoModelFactory):
+    FACTORY_FOR = settings.AUTH_USER_MODEL
+
+
+class GalleryFactory(factory.django.DjangoModelFactory):
+    FACTORY_FOR = Gallery
+    author = factory.SubFactory(UserFactory)
 
 
 class GalleryModelTestCase(unittest.TestCase):
