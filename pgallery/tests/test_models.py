b9841892b8bd4d76f25dfc22e4bc2743f5609718
Zbigniew Siciarz <antyqjon@gmail.com>
2014-06-21 22:14:57 +0200

    Updated tests for factory-boy Meta class.

--- pgallery/tests/test_models.py
+++ pgallery/tests/test_models.py
@@ -11,19 +11,24 @@ from ..models import Gallery, Photo
 
 
 class UserFactory(factory.django.DjangoModelFactory):
-    FACTORY_FOR = settings.AUTH_USER_MODEL
+    class Meta:
+        model = settings.AUTH_USER_MODEL
 
 
 class GalleryFactory(factory.django.DjangoModelFactory):
-    FACTORY_FOR = Gallery
     author = factory.SubFactory(UserFactory)
+
+    class Meta:
+        model = Gallery
 
 
 class PhotoFactory(factory.django.DjangoModelFactory):
-    FACTORY_FOR = Photo
     gallery = factory.SubFactory(GalleryFactory)
     author = factory.LazyAttribute(lambda obj: obj.gallery.author)
     image = factory.django.ImageField(width=1024, height=768)
+
+    class Meta:
+        model = Photo
 
 
 class GalleryModelTestCase(unittest.TestCase):
