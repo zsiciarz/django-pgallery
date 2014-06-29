3959028915ab61afb8940755b8e9fbc1abd63057
Zbigniew Siciarz <antyqjon@gmail.com>
2014-06-29 11:15:29 +0200

    We don't need nose and django-nose anymore.

--- tests/settings.py
+++ tests/settings.py
@@ -19,7 +19,6 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django_nose',
     'markitup',
     'sorl.thumbnail',
     'pgallery',
