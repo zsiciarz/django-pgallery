f3c2049874cff86a38105e0f63013c728913d065
Zbigniew Siciarz <antyqjon@gmail.com>
2014-07-26 13:21:10 +0200

    Added MIDDLEWARE_CLASSES to test settings for Django 1.7+ compatibility.

--- tests/settings.py
+++ tests/settings.py
@@ -24,6 +24,15 @@ INSTALLED_APPS = (
     'pgallery',
 )
 
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
 SECRET_KEY = 'notreallyasecret'
 
 MEDIA_ROOT = project_path('media')
