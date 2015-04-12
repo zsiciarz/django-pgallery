f620dde75d65e1175829b524eec00d54e20bb2be
Zbigniew Siciarz <zbigniew@siciarz.net>
2015-04-12 22:17:01 +0200

    Test drafts visible for staff users only.

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,8 +1,28 @@
 from __future__ import unicode_literals
 
+from django.contrib.auth.models import AnonymousUser
+
 from djet.testcases import ViewTestCase
 
-from pgallery.views import TaggedPhotoListView
+from pgallery.views import GalleryListView, TaggedPhotoListView
+from .factories import GalleryFactory, UserFactory
+
+
+class GalleryListViewTestCase(ViewTestCase):
+    view_class = GalleryListView
+
+    def test_draft_invisible(self):
+        gallery = GalleryFactory(status='draft', title="Draft gallery")
+        request = self.factory.get(user=AnonymousUser())
+        response = self.view(request)
+        self.assertNotContains(response, gallery.title)
+
+    def test_draft_visible_for_staff(self):
+        gallery = GalleryFactory(status='draft', title="Draft gallery")
+        user = UserFactory(is_staff=True)
+        request = self.factory.get(user=user)
+        response = self.view(request)
+        self.assertContains(response, gallery.title)
 
 
 class TaggedPhotoListViewTestCase(ViewTestCase):
