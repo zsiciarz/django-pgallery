db51a947763daaf4907a4c07024fd54aa1c0054e
Zbigniew Siciarz <zbigniew@siciarz.net>
2017-06-19 21:24:06 +0200

    Fix initial migration not having on_delete on FKs.

--- pgallery/migrations/0001_initial.py
+++ pgallery/migrations/0001_initial.py
@@ -30,7 +30,7 @@ class Migration(migrations.Migration):
                 ('description', markitup.fields.MarkupField(no_rendered_field=True, verbose_name='description')),
                 ('shot_date', models.DateField(null=True, verbose_name='shot date', blank=True)),
                 ('_description_rendered', models.TextField(editable=False, blank=True)),
-                ('author', models.ForeignKey(editable=False, to=settings.AUTH_USER_MODEL, verbose_name='author')),
+                ('author', models.ForeignKey(editable=False, to=settings.AUTH_USER_MODEL, verbose_name='author', on_delete=models.CASCADE)),
             ],
             options={
                 'ordering': ['-shot_date'],
@@ -47,8 +47,8 @@ class Migration(migrations.Migration):
                 ('image', models.ImageField(upload_to='photos/%Y/%m/%d', verbose_name='image')),
                 ('tags', django.contrib.postgres.fields.ArrayField(base_field=models.CharField(max_length=64), size=None)),
                 ('exif', django.contrib.postgres.fields.hstore.HStoreField(default={}, editable=False, db_index=True)),
-                ('author', models.ForeignKey(editable=False, to=settings.AUTH_USER_MODEL, verbose_name='author')),
-                ('gallery', models.ForeignKey(related_name='photos', verbose_name='gallery', to='pgallery.Gallery', null=True)),
+                ('author', models.ForeignKey(editable=False, to=settings.AUTH_USER_MODEL, verbose_name='author', on_delete=models.CASCADE)),
+                ('gallery', models.ForeignKey(related_name='photos', verbose_name='gallery', to='pgallery.Gallery', null=True, on_delete=models.CASCADE)),
             ],
             options={
                 'ordering': ['created'],
