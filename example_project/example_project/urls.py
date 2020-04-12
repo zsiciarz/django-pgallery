989abdc718973551bbb3565859d75ea0408776d0
Zbigniew Siciarz <zbigniew@siciarz.net>
2020-04-12 10:45:46 +0200

    Fix URLconf for example project.

--- example_project/example_project/urls.py
+++ example_project/example_project/urls.py
@@ -7,7 +7,8 @@ urlpatterns = [
     # Examples:
     # url(r'^$', 'example_project.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r"^admin/", include(admin.site.urls)),
+    url(r"^admin/", admin.site.urls),
     url(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
+    url(r"^markitup/", include("markitup.urls")),
     url(r"", include("pgallery.urls", namespace="pgallery")),
 ]
