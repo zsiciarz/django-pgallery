c1225a01fd1d5540727579e440ba01a66f8fa512
Zbigniew Siciarz <zbigniew@siciarz.net>
2021-09-01 19:45:02 +0200

    Fix deprecation warnings on Django 3.1

--- tests/urls.py
+++ tests/urls.py
@@ -1,5 +1,5 @@
-from django.conf.urls import include, url
+from django.urls import include, path
 
 urlpatterns = [
-    url(r"", include("pgallery.urls", namespace="pgallery")),
+    path("", include("pgallery.urls", namespace="pgallery")),
 ]
